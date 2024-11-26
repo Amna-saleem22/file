1e1f91f436fa85b552daf847a7533171ea00594e
Amna-Saleem22 <saleemamna431@gmail.com>
2024-11-26 15:34:31 +0500

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,9 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-
+interface employes{
+  name: string;
+  age: string;
+};
 @Component({
   selector: 'app-root',
   imports: [RouterOutlet],
@@ -9,4 +12,27 @@ import { RouterOutlet } from '@angular/router';
 })
 export class AppComponent {
   title = 'AngularAppLearning';
+
+
+ logged:boolean = false;
+
+login() :void{
+  if(this.logged){
+    
+  }
+   
+}
+ logout() :void{
+  this.logged =!this.logged;
+ }
+
+
+ namee:string = "Abdullah";
+company:employes[] = [
+{name:"amna",age:"55"},
+{name:"hina",age:"38"},
+{name:"ayesha",age:"45"},
+{name:"hiba",age:"25"},
+{name:"huma",age:"35"},
+];
 }
